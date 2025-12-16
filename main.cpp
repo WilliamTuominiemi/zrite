e350a737d4dbe6386924067e2d484b4ed21e87f3
WilliamTuominiemi <william.m.tuominiemi@gmail.com>
2025-12-16 22:49:02 +0200

    play sound using sfml

--- main.cpp
+++ main.cpp
@@ -1,7 +1,22 @@
+#include <SFML/Audio.hpp>
 #include <iostream>
+#include <thread>
+#include <chrono>
 
 int main()
 {
-    std::cout << "test" << std::endl;
+    sf::SoundBuffer buffer;
+    if (!buffer.loadFromFile("../sound.wav"))
+        return -1;
+
+    sf::Sound sound;
+    sound.setBuffer(buffer);
+    sound.play();
+
+    while (sound.getStatus() == sf::Sound::Playing)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+
     return 0;
 }
